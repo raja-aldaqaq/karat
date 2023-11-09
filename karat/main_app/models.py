0ae78b4b9d1a180cfdafc645314729708ad2f40d
Zainab <zainabsameer197@gmail.com>
2023-11-09 12:43:07 +0300

    add shop

--- karat/main_app/models.py
+++ karat/main_app/models.py
@@ -4,8 +4,7 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-# class User(models.Model):
-#     user = models.ForeignKey(User , on_delete=models.CASCADE)
+
 
 
 class Shop(models.Model):
